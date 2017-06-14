de9b28cf834a00651153fa763f3b27a7057cbb8d
Denis Dekeyzer <ddekeyzer@aspaway.fr>
2017-06-14 18:07:53 +0200

    Now handeling unknown number of screens.

--- i3fun.sh
+++ i3fun.sh
@@ -2,8 +2,14 @@
 
 import -window root /tmp/prtscrn.png
 convert /tmp/prtscrn.png -blur 0x5 /tmp/prtscrn.png
-hw=`xrandr | grep '*' | head -n 1 | cut -d ' ' -f4 | cut -d 'x' -f1 | awk '{print $1/2}'`
-convert -gravity Center -weight 700 -pointsize 60 -stroke white -strokewidth 1.5 -annotate -$hw-0 "$(fortune | fold -s -50)"  /tmp/prtscrn.png /tmp/prtscrn.png 
-convert -gravity Center -weight 700 -pointsize 60 -stroke white -strokewidth 1.5 -annotate +$hw-0 "$(fortune | fold -s -50)"  /tmp/prtscrn.png /tmp/prtscrn.png 
+
+i=0
+tot=$((`xrandr | grep -c '*'` - 1))
+xrandr | grep '*' | cut -d ' ' -f4 | cut -d 'x' -f1 | while read -r line ; do
+    hw=$(($(($i * 2 - $tot)) * $line / 2))
+    if [ $hw -ge 0 ]; then hw="+$hw"; fi
+    convert -gravity Center -weight 700 -pointsize 60 -stroke white -strokewidth 1.5 -annotate $hw-0 "$(fortune | fold -s -50)"  /tmp/prtscrn.png /tmp/prtscrn.png
+    i=$(($i + 1))
+done
 i3lock -i /tmp/prtscrn.png
 rm /tmp/prtscrn.png
