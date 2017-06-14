cdb1a403a9e7b2d28b7f81bb6dcf768d874e0d35
Denis Dekeyzer <ddekeyzer@aspaway.fr>
2017-06-14 18:15:16 +0200

    Updated var and file names

--- i3fun.sh
+++ i3fun.sh
@@ -6,9 +6,9 @@ convert /tmp/prtscrn.png -blur 0x5 /tmp/prtscrn.png
 i=0
 tot=$((`xrandr | grep -c '*'` - 1))
 xrandr | grep '*' | cut -d ' ' -f4 | cut -d 'x' -f1 | while read -r line ; do
-    hw=$(($(($i * 2 - $tot)) * $line / 2))
-    if [ $hw -ge 0 ]; then hw="+$hw"; fi
-    convert -gravity Center -weight 700 -pointsize 60 -stroke white -strokewidth 1.5 -annotate $hw-0 "$(fortune | fold -s -50)"  /tmp/prtscrn.png /tmp/prtscrn.png
+    shift=$(($(($i * 2 - $tot)) * $line / 2))
+    if [ $shift -ge 0 ]; then shift="+$shift"; fi
+    convert -gravity Center -weight 700 -pointsize 60 -stroke white -strokewidth 1.5 -annotate $shift-0 "$(fortune | fold -s -50)"  /tmp/prntscrn.png /tmp/prntscrn.png
     i=$(($i + 1))
 done
 i3lock -i /tmp/prtscrn.png
