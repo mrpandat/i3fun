3aac5dc3e957a86b22efe99918e15ba56c5882cf
KagXaef <mrpandat@hotmail.fr>
2018-09-04 13:51:09 +0200

    Add only short quotes

--- i3fun.sh
+++ i3fun.sh
@@ -1,2 +1,2 @@
-convert -size 1920x1080 xc:White -gravity Center -weight 700 -pointsize 60  -annotate 0 "$(fortune | fold -s -65)" /tmp/i3fun.png && i3lock -i /tmp/i3fun.png
+convert -size 1920x1080 xc:White -gravity Center -weight 700 -pointsize 60  -annotate 0 "$(fortune -s | fold -s -65)" /tmp/i3fun.png && i3lock -i /tmp/i3fun.png
 
